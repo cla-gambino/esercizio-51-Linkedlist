3886585de29a28fecc37a8d1567a7fc98198b2f5
cla-gambino <154328607+cla-gambino@users.noreply.github.com>
2024-02-07 18:28:29 +0100

    addFirst and addLast fruits

--- src/Fruit.java
+++ src/Fruit.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Fruit {
+public class Fruit {
+
+    public String name;
+
+    public Fruit(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Fruit{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        LinkedList<Fruit> fruit = new LinkedList<>();
+
+        fruit.add(new Fruit("Arancia"));
+        fruit.add(new Fruit("Banana"));
+        fruit.add(new Fruit("Fragola"));
+        fruit.add(new Fruit("Anguria"));
+        fruit.add(new Fruit("Mandarino"));
+        fruit.add(new Fruit("Mela"));
+        fruit.add(new Fruit("Ciliegia"));
+        fruit.add(new Fruit("Albicocca"));
+
+        System.out.println("lista di frutti: " + fruit);
+
+        fruit.addFirst(new Fruit("Ananas"));
+        fruit.addLast(new Fruit("Kiwi"));
+
+        System.out.println("lista di frutti aggiornata: " + fruit);
+
     }
 }
